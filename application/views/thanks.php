9fe234d10c1f716f98820903f4f7d32ff342f80c
unknown <parlin03@gmail.com>
2024-03-18 01:08:49 +0800

    thanks

---
+++ application/views/thanks.php
@@ -0,0 +1,26 @@
+<?php header('Refresh:10; url= ' . base_url()); ?>
+<div class="container">
+
+    <div class="card o-hidden border-0 shadow-lg my-5 col-lg-6 mx-auto">
+        <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+                <div class="col-lg">
+                    <div class="p-5">
+                        <div class="text-center">
+                            <h1 class="h4 text-gray-900 mb-4">
+                                TERIMA KAZIH ZENIOR ZUDAH MENDAFTAR<br>
+                                Tiada Kezan Tanpa Kehadiranta’ Kanda <?= strtoupper($name); ?>
+                                <br>
+                                <img class="ae-4" width="350" src="<?= base_url(); ?>/assets/home/img/hbh-home-dark.png" alt=" HBH IKATEK UH<br>TRANSFORMATIVE 2024<br>12-13 April 2024" data-action="zoom" />
+
+                            </h1>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
