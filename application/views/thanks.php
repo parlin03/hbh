3cfbd8a0c188847b12b6dea9ffc743df12bce07c
unknown <parlin03@gmail.com>
2024-03-19 17:40:04 +0800

    thanks

--- application/views/thanks.php
+++ application/views/thanks.php
@@ -8,13 +8,16 @@
                 <div class="col-lg">
                     <div class="p-5">
                         <div class="text-center">
-                            <h1 class="h4 text-gray-900 mb-4">
-                                TERIMA KAZIH ZENIOR ZUDAH MENDAFTAR<br>
-                                Tiada Kezan Tanpa Kehadiranta’ Kanda <?= strtoupper($name); ?>
-                                <br>
-                                <img class="ae-4" width="200" src="<?= base_url(); ?>/assets/home/img/hbh-home-dark.png" alt=" HBH IKATEK UH<br>TRANSFORMATIVE 2024<br>12-13 April 2024" data-action="zoom" />
-
+                            <h1 class="h4 text-gray-900 mb-3">
+                                TERIMA KAZIH ZENIOR ZUDAH MENDAFTAR
                             </h1>
+                            <p class="h4 text-gray-900 mb-4">Tiada Kezan Tanpa Kehadiranta’ Kanda <br>
+                                <?= strtoupper($name); ?>
+                            </p>
+                            <span>
+
+                                <img class="ae-4" width="200" src="<?= base_url(); ?>/assets/home/img/hbh-home-dark.png" alt=" HBH IKATEK UH<br>TRANSFORMATIVE 2024<br>12-13 April 2024" data-action="zoom" />
+                            </span>
                         </div>
 
                     </div>
